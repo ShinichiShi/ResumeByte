f9fc231ae9d536cbe7877fb2ee2530a0ce6a99cd
ShinichiShi <supreeth2020@gmail.com>
2024-07-09 21:07:08 +0530

    add fonts

---
+++ src/components/Resume Display/Personal.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+const Personal = ({ personalDetails }) => {
+  const { name, email, phoneNumber, address } = personalDetails;
+  return (
+    <div className="bg-slate-400 h-[16vh] flex items-center justify-center flex-col gap-[2vh]">
+      {name!=='' && <div className="flex items-center justify-center text-5xl">{name}</div>}
+      <div className="flex items-center justify-around flex-row gap-10  ">
+        {email!==''&&
+        <div className='flex items-center justify-center gap-2'>
+           <i class="fa-solid fa-envelope" />
+           <p>{email}</p>
+        </div>}
+        {phoneNumber!==''&&
+        <div className='flex items-center justify-center gap-2'>
+           <i class="fa-solid fa-phone" />
+           <p>{phoneNumber}</p>
+        </div>}
+        {address!==''&&
+        <div className='flex items-center justify-center gap-2'>
+           <i class="fa-solid fa-location-dot" />
+           <p>{address}</p>
+        </div>}
+      </div>
+    </div>
+  );
+};
+
+export default Personal;
