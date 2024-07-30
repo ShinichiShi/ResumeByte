1817dacdaaed21b8b807d485e2d80ac7dd1d9c69
ShinichiShi <supreeth2020@gmail.com>
2024-07-30 18:17:47 +0530

    add react icons

--- src/components/Resume Display/Personal.jsx
+++ src/components/Resume Display/Personal.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import { IoMdMail } from "react-icons/io";
-import { FaPhoneAlt } from "react-icons/fa";
-import { FaLocationDot } from "react-icons/fa6";
-const Personal = ({ personalDetails }) => {
+import { IoMdMail } from 'react-icons/io';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { FaLocationDot } from 'react-icons/fa6';
+
+const Personal = ({ personalDetails, style }) => {
   const { name, email, phoneNumber, address } = personalDetails;
+
+  const containerClasses = style === 'informal' 
+    ? 'bg-slate-400 h-[16vh] flex items-center justify-center flex-col gap-[2vh]'
+    : 'h-20 flex items-center justify-center flex-col gap-3';
+
+  const nameClasses = style === 'informal' 
+    ? 'flex items-center justify-center text-5xl uppercase'
+    : 'flex items-center justify-center text-3xl uppercase';
+
   return (
-    <div className="bg-slate-400 h-[16vh] flex items-center justify-center flex-col gap-[2vh]">
+    <div className={containerClasses}>
       {name !== '' && (
-        <div className="flex items-center justify-center text-5xl">{name}</div>
+        <div className={nameClasses}>{name}</div>
       )}
-      <div className="flex items-center justify-around flex-row gap-10  ">
+      <div className="flex items-center justify-around flex-row gap-10">
         {email !== '' && (
           <div className="flex items-center justify-center gap-2">
             <IoMdMail />
@@ -18,13 +28,13 @@ const Personal = ({ personalDetails }) => {
         )}
         {phoneNumber !== '' && (
           <div className="flex items-center justify-center gap-2">
-            <FaPhoneAlt/>
+            <FaPhoneAlt />
             <p>{phoneNumber}</p>
           </div>
         )}
         {address !== '' && (
           <div className="flex items-center justify-center gap-2">
-            <FaLocationDot/>
+            <FaLocationDot />
             <p>{address}</p>
           </div>
         )}
